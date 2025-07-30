e4eaa1d8ae71329b856583737140902787fb7616
cfkubo <arul.vannala@gmail.com>
2025-07-30 17:09:37 -0600

    test view

---
+++ src/main/java/rmqconsumer/example/rmqconsumer/views/HomeView.java
@@ -0,0 +1,31 @@
+package rmqconsumer.example.rmqconsumer.views;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+import rmqconsumer.example.rmqconsumer.service.RabbitMQStatsService;
+import rmqconsumer.example.rmqconsumer.service.TransactionConsumerService;
+
+@Route("")
+public class HomeView extends VerticalLayout {
+
+    @Autowired
+    public HomeView(RabbitMQStatsService statsService) {
+        add(new H1("RabbitMQ Consumer Dashboard"));
+
+        add(new Paragraph("Consuming from queues:"));
+
+        String[] queues = {"classic.transactions", "quorum.transactions", "stream.transactions"};
+        for (String queue : queues) {
+            int count = statsService.getMessageCount(queue);
+            int consumed = 0;
+            if (queue.equals("classic.transactions")) consumed = TransactionConsumerService.classicConsumed.get();
+            if (queue.equals("quorum.transactions")) consumed = TransactionConsumerService.quorumConsumed.get();
+            if (queue.equals("stream.transactions")) consumed = TransactionConsumerService.streamConsumed.get();
+
+            add(new Paragraph(queue + " - Messages in queue: " + count + ", Consumed: " + consumed));
+        }
+    }
+}
