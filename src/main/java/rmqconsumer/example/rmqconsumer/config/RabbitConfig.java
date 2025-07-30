93d8ea0298fd8f51a90460476c870f317031d1ff
cfkubo <arul.vannala@gmail.com>
2025-07-30 17:08:43 -0600

    rmq config

---
+++ src/main/java/rmqconsumer/example/rmqconsumer/config/RabbitConfig.java
@@ -0,0 +1,21 @@
+package rmqconsumer.example.rmqconsumer.config;
+
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setAcknowledgeMode(org.springframework.amqp.core.AcknowledgeMode.MANUAL);
+        factory.setPrefetchCount(50); // fetch more messages at once
+        factory.setConcurrentConsumers(3); // use more threads
+        factory.setMaxConcurrentConsumers(10);
+        return factory;
+    }
+}
