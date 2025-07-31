c3c34384092e50b3ac18d4e82a9a4e179671d787
cfkubo <arul.vannala@gmail.com>
2025-07-30 18:11:05 -0600

    testing

--- src/main/java/rmqconsumer/example/rmqconsumer/service/RabbitMQStatsService.java
+++ src/main/java/rmqconsumer/example/rmqconsumer/service/RabbitMQStatsService.java
@@ -1,25 +1,25 @@
-package rmqconsumer.example.rmqconsumer.service;
+// package rmqconsumer.example.rmqconsumer.service;
 
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.stereotype.Service;
+// import org.springframework.amqp.rabbit.core.RabbitAdmin;
+// import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+// import org.springframework.stereotype.Service;
 
-import java.util.Map;
+// import java.util.Map;
 
-@Service
-public class RabbitMQStatsService {
+// @Service
+// public class RabbitMQStatsService {
 
-    private final RabbitAdmin rabbitAdmin;
+//     private final RabbitAdmin rabbitAdmin;
 
-    public RabbitMQStatsService(ConnectionFactory connectionFactory) {
-        this.rabbitAdmin = new RabbitAdmin(connectionFactory);
-    }
+//     public RabbitMQStatsService(ConnectionFactory connectionFactory) {
+//         this.rabbitAdmin = new RabbitAdmin(connectionFactory);
+//     }
 
-    public int getMessageCount(String queueName) {
-        Map<Object props = rabbitAdmin.getQueueProperties(queueName);
-        if (props != null && props.containsKey("QUEUE_MESSAGE_COUNT")) {
-            return (int) props.get("QUEUE_MESSAGE_COUNT");
-        }
-        return -1;
-    }
-}
+//     public int getMessageCount(String queueName) {
+//         Map<Object props = rabbitAdmin.getQueueProperties(queueName);
+//         if (props != null && props.containsKey("QUEUE_MESSAGE_COUNT")) {
+//             return (int) props.get("QUEUE_MESSAGE_COUNT");
+//         }
+//         return -1;
+//     }
+// }
